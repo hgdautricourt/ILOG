7f5bad270198e03adb70c574f2b4bf168caa7482
HugoDerigny <hugo.derigny@free.fr>
2022-12-05 15:48:33 +0100

    feat: update express files

--- back/index.ts
+++ back/index.ts
@@ -22,19 +22,17 @@ io.on('connect', (socket) => {
 app.use(userAgent.express());
 
 app.get('/', (req: Request, res: Response) => {
-    res.sendFile(req.useragent?.isMobile ? './laserpointer.html' : './slides.html', { root: "public" });
+    res.send('Hello world')
+    // res.sendFile(req.useragent?.isMobile ? './laserpointer.html' : './slides.html', { root: "public" });
 })
 
-app.get('/slides', (req: Request, res: Response) => {
+app.get('/api/slides', (req: Request, res: Response) => {
     res.sendFile('./slides.html', { root: "public" });
 })
 
-app.get('/laserpointer', (req: Request, res: Response) => {
+app.get('/api/laserpointer', (req: Request, res: Response) => {
     res.sendFile('./laserpointer.html', { root: "public" });
 })
-
-// app.use(express.static("dist/public"));
-
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
